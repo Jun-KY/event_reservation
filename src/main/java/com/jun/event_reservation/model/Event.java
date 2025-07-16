28cb1fdfabc65a77e5d453f90b94367c8d9ed1e1
Youngjun Kwon <Youngjun.Kwon@students.mq.edu.au>
2025-07-16 09:44:09 +0900

    First commit create Model

---
+++ src/main/java/com/jun/event_reservation/model/Event.java
@@ -0,0 +1,29 @@
+package com.jun.event_reservation.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity @Getter @Setter @Table(name= "events")
+@NoArgsConstructor @AllArgsConstructor
+public class Event {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String name;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDateTime eventDate;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Reservation> reservations = new ArrayList<>();
+}
---
+++ src/main/java/com/jun/event_reservation/model/Reservation.java
@@ -0,0 +1,26 @@
+package com.jun.event_reservation.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity @Getter @Setter @Table(name = "reservations")
+@NoArgsConstructor @AllArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String attendeeName;
+
+    private int seat;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    @JsonBackReference
+    private Event event;
+}
